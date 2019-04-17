10c1a34fe50684bf349905ee718d8250a98f3938
Syow <crescentsyou@gmail.com>
2019-04-17 11:48:34 +0800

    exe_res

---
+++ README.md
@@ -0,0 +1,12 @@
+# npstore_exe
+無人商店結帳程式
+
+QRcode
+---------
+use instascan.min.js 
+https://github.com/schmich/instascan
+
+GUI build-Electron
+-----------------
+build cross platform GUI desktop apps with javascript+html
+https://electronjs.org/
---
+++ electron/app.js
@@ -0,0 +1,32 @@
+const electron = require('electron') ;
+const app = electron.app ;
+const browser = electron.BrowserWindow ;
+
+let mainwindow ;
+
+function createwindow(){
+    mainwindow = new browser({
+        resizable:false ,
+        title:'無人商店結帳系統' ,
+        icon:'logo.png'
+    });
+    electron.Menu.setApplicationMenu(null) ;
+    mainwindow.loadFile('qrweb.html') ;
+    //mainwindow.webContents.openDevTools() ;
+    mainwindow.setFullScreen(true) ;
+    mainwindow.on('closed',function(){
+        mainwindow = null ;
+    }) ;
+}
+
+app.on('ready' , createwindow) ;
+
+app.on('window-all-closed',function(){
+    app.quit() ;
+});
+
+app.on('active' , function(){
+    if( mainwindow == null ){
+        createwindow() ;
+    }
+});
